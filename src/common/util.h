7d63fac1db0a75ab4759a1a25da35e7bdca71992
JelsikJozef <jozefjelsik@gmail.com>
2025-12-26 13:23:19 +0100

    util.c a util.h

--- src/common/util.h
+++ src/common/util.h
@@ -4,9 +4,19 @@
 
 #ifndef SEMPRACA_UTIL_H
 #define SEMPRACA_UTIL_H
+/*
+ *util.h - simple helper functions for the project
+ *
+ *purpose is simpli logging and error handling
+ */
 
-void die(const char *msg);
+/*logs an error message and exits the program*/
+void die(const char *fmt, ...);
+
+/*logs an informational message*/
 void log_info(const char *fmt, ...);
+
+/*logs a warning message*/
 void log_error(const char *fmt, ...);
 
 #endif //SEMPRACA_UTIL_H
